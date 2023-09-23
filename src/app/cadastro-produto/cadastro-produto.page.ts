c062f5668996292bb26a92ff06dc3242f63a8db9
erlanbelo21 <erlanbelo@hotmail.com>
2023-09-22 22:18:48 -0300

    fim do projeto

--- src/app/cadastro-produto/cadastro-produto.page.ts
+++ src/app/cadastro-produto/cadastro-produto.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ProdutoService } from '../services/produto.service';
+import { Produto } from '../Models/Produto.model';
 
 @Component({
-  selector: 'app-cadastro-produto',
-  templateUrl: './cadastro-produto.page.html',
-  styleUrls: ['./cadastro-produto.page.scss'],
+  selector: 'app-cadastro-Produto',
+  templateUrl: './cadastro-Produto.page.html',
+  styleUrls: ['./cadastro-Produto.page.scss'],
 })
-export class CadastroProdutoPage implements OnInit {
+export class CadastroProdutoPage {
 
-  constructor() { }
+  Produto: Produto = {
+    nome: '',
+    descricao: '',
+    validade: '',
+    preco: 0,
+  };
 
-  ngOnInit() {
+  constructor(private produtoService: ProdutoService) { }
+
+  salvarProduto() {
+    if (this.Produto.nome && this.Produto.descricao && this.Produto.preco >= 0) {
+     
+      if (!this.Produto.validade) {
+        this.Produto.validade = '01/01/2030';
+      }
+
+      this.produtoService.salvar(this.Produto).subscribe((retorno) => {
+        this.Produto = retorno;
+        alert(`Sucesso! Produto [${this.Produto.id}] foi salvo.`);
+      });
+    } else {
+      alert("Preencha todos os campos corretamente.");
+    }
   }
-
 }
